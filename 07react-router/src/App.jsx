284237faf12a0c2d22691d73d2eef647f32dfd3b
Sakshyam Shrestha <sachhyam28@gmail.com>
2025-04-10 17:54:45 +0545

    learning react-router

---
+++ 07react-router/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='w-full m-5 rounded-2xl bg-amber-200 text-center font-bold text-black p-2'>React router</h1>
+    </>
+  )
+}
+
+export default App
